e9cfba053b2d3e386b49a7c0c7982dcd0dafcf93
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-12-04 14:17:03 +0200

    notes js

---
+++ arrays/reverseArray.js
@@ -0,0 +1,5 @@
+/*
+Write a program which receives a number n and an array of elements. Your task is to create a new array with n numbers, reverse it and print its elements on a single line, space separated.
+*/
+
+function reverse(num, array) {}
